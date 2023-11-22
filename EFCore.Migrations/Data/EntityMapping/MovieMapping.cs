2c33717e3350323af7be21bfaa6312e0d033a1ce
TJ Lopez <wtjlopez99@gmail.com>
2023-11-21 20:55:08 -0500

    First Migration created

--- EFCore.Migrations/Data/EntityMapping/MovieMapping.cs
+++ EFCore.Migrations/Data/EntityMapping/MovieMapping.cs
@@ -42,25 +42,5 @@ public class MovieMapping : IEntityTypeConfiguration<Movie>
             .WithMany(genre => genre.Movies)
             .HasPrincipalKey(genre => genre.Id)
             .HasForeignKey(movie => movie.MainGenreId);
-        
-        // Seed - data that needs to be created always
-        builder.HasData(new Movie
-        {
-            Id = 1,
-            Title = "Fight Club",
-            ReleaseDate = new DateTime(1999, 9, 10),
-            Synopsis = "Ed Norton and Brad Pitt have a couple of fist fights with each other.",
-            MainGenreId = 1,
-            AgeRating = AgeRating.Adolescent
-        });
-
-        builder.OwnsOne(movie => movie.Director)
-            .HasData(new { MovieId = 1, FirstName = "David", LastName = "Fincher" });
-
-        builder.OwnsMany(movie => movie.Actors)
-            .HasData(
-                new { MovieIdentifier = 1, Id = 1, FirstName = "Edward", LastName = "Norton" },
-                new { MovieIdentifier = 1, Id = 2, FirstName = "Brad", LastName = "Pitt" }
-            );
     }
 }
---
+++ EFCore.Migrations/Migrations/20231122015220_InitialSchema.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCore.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Genres",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genres", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "varchar(128)", maxLength: 128, nullable: false),
+                    ReleaseDate = table.Column<string>(type: "char(8)", nullable: false),
+                    Plot = table.Column<string>(type: "varchar(max)", nullable: true),
+                    AgeRating = table.Column<string>(type: "varchar(32)", nullable: false),
+                    MainGenreId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Movies_Genres_MainGenreId",
+                        column: x => x.MainGenreId,
+                        principalTable: "Genres",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Movie_Actors",
+                columns: table => new
+                {
+                    MovieId = table.Column<int>(type: "int", nullable: false),
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movie_Actors", x => new { x.MovieId, x.Id });
+                    table.ForeignKey(
+                        name: "FK_Movie_Actors_Movies_MovieId",
+                        column: x => x.MovieId,
+                        principalTable: "Movies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Movie_Directors",
+                columns: table => new
+                {
+                    MovieId = table.Column<int>(type: "int", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movie_Directors", x => x.MovieId);
+                    table.ForeignKey(
+                        name: "FK_Movie_Directors_Movies_MovieId",
+                        column: x => x.MovieId,
+                        principalTable: "Movies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movies_MainGenreId",
+                table: "Movies",
+                column: "MainGenreId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movie_Actors");
+
+            migrationBuilder.DropTable(
+                name: "Movie_Directors");
+
+            migrationBuilder.DropTable(
+                name: "Movies");
+
+            migrationBuilder.DropTable(
+                name: "Genres");
+        }
+    }
+}
