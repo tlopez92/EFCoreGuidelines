bb15edee8f5416f32f4b406189427ca8c28dd315
TJ Lopez <wtjlopez99@gmail.com>
2023-11-22 11:59:04 -0500

    integration test added

---
+++ EfCore.Testing.Tests/Integration/IntegrationTest.cs
@@ -0,0 +1,51 @@
+using EfCore.Testing.SimpleApi.Data;
+using EfCore.Testing.SimpleApi.Models;
+using EfCore.Testing.SimpleApi.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfCore.Testing.Tests.Integration;
+
+public class IntegrationTest
+{
+    private readonly MoviesContext _testContext;
+    private readonly MoviesContext _verificationContext;
+
+    public IntegrationTest()
+    {
+        var options = new DbContextOptionsBuilder<MoviesContext>()
+            .UseSqlServer("""
+                          Data Source=localhost;
+                          Initial Catalog=TestDB;
+                          User Id=sa;
+                          Password=Password123;
+                          TrustServerCertificate=True;
+                          """)
+            .Options;
+
+        _testContext = new MoviesContext(options);
+        _verificationContext = new MoviesContext(options);
+
+        // Usually not needed, as a deploy of the test database
+        // before the test run will migrate the schema.
+        _testContext.Database.EnsureDeleted();
+        _testContext.Database.EnsureCreated();
+    }
+    
+    [Fact]
+    public async Task WhenGenreCreated_GenreIsInDatabase()
+    {
+        // Arrange
+        var repository = new GenreRepository(_testContext);
+        
+        // Act
+        var genreToCreate = new Genre { Name = "MyAwesomeGenre" };
+        var createdGenre = await repository.Create(genreToCreate);
+        
+        // Assert
+        Assert.NotNull(createdGenre);
+        Assert.True(createdGenre.Id > 0);
+        var verificationGenre = _verificationContext.Genres.Find(createdGenre.Id);
+        Assert.NotNull(verificationGenre);
+        Assert.Equal(genreToCreate.Name, verificationGenre.Name);
+    }
+}
